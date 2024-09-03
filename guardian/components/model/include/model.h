f502b111c03c4cfab5c9c60ced3d32262f0a8931
mudderschiff <gabriel.cmiel@uni-a.de>
2024-09-03 13:55:44 +0200

    wolfssl shared component

--- guardian/components/model/include/model.h
+++ guardian/components/model/include/model.h
@@ -1,1 +1,19 @@
-void func(void);
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <wolfssl/wolfcrypt/port/Espressif/esp32-crypt.h>
+#include <wolfssl/wolfcrypt/wolfmath.h>
+#include <wolfssl/ssl.h>
+#include "esp_log.h"
+#include "esp_random.h"
+#include "model.h"
+
+
+int mod_exp(sp_int *g, sp_int *x, sp_int *p, sp_int *y);
+void print_mp_int(sp_int *num);
+int g_pow_p(sp_int *seckey, sp_int *pubkey);
+int rand_q(sp_int *result);
+int hash(sp_int *a, sp_int *b, sp_int *result);
+int make_schnorr_proof(sp_int *pubkey, sp_int *seckey);
+
+#endif // MOD_MATH_H
--- guardian/main/main.c
+++ guardian/main/main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include "model.h"
+#include "freertos/task.h"
+#include "freertos/FreeRTOS.h"
 
-void app_main(void)
-{
-
+void app_main(void) {
+    DECL_MP_INT_SIZE(seckey, 256);
+    DECL_MP_INT_SIZE(pubkey, 3072);
+    NEW_MP_INT_SIZE(seckey, 256, NULL, DYNAMIC_TYPE_BIGINT);
+    NEW_MP_INT_SIZE(pubkey, 3072, NULL, DYNAMIC_TYPE_BIGINT);
+    INIT_MP_INT_SIZE(seckey, 256);
+    INIT_MP_INT_SIZE(pubkey, 3072);
+    rand_q(seckey);
+    g_pow_p(seckey, pubkey);
+    make_schnorr_proof(seckey, pubkey);
+    
+    
+    //Clear
+    sp_zero(seckey);
+    sp_zero(pubkey);
+    FREE_MP_INT_SIZE(seckey, NULL, DYNAMIC_TYPE_BIGINT);
+    FREE_MP_INT_SIZE(pubkey, NULL, DYNAMIC_TYPE_BIGINT);
 }
