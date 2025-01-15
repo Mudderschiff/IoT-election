3a69bf3c926286fb2819e0bcf93e101d6d859242
mudderschiff <gabriel.cmiel@uni-a.de>
2025-01-15 20:19:43 +0100

    +- Python: Overwrite python constants because there are no Reduced Parameters ++ Python: Generates example Ballots which are then cast ++ Python: Encryption of all Ballots into an Encrypted Tally

--- initBallotBox/election.py
+++ initBallotBox/election.py
@@ -1,24 +1,30 @@
 import json
+import electionguard
 import binascii
 import paho.mqtt.client as mqtt
 from datetime import datetime, timedelta
 from typing import List, Dict
+from random import randint
+import logging
+
 
 from electionguard import SpecVersion, ElectionType, GeopoliticalUnit, ReportingUnitType, Party, Candidate, BallotStyle, \
     SelectionDescription, VoteVariationType, \
     ContestDescription
 
 from electionguard.type import BallotId
+from electionguard.utils import get_optional
 
 # Step 0 - Configure Election
-from electionguard.constants import ElectionConstants
+from electionguard.constants import ElectionConstants, create_constants
+
 from electionguard.election import CiphertextElectionContext
 from electionguard.manifest import Manifest, InternalManifest
-import electionguard.group
 
 # Step 1 - Key Ceremony
-from electionguard.guardian import Guardian, GuardianRecord, PrivateGuardianRecord
-from electionguard.key_ceremony_mediator import KeyCeremonyMediator
+from electionguard.group import hex_to_p, hex_to_q
+#from electionguard.guardian import Guardian, GuardianRecord, PrivateGuardianRecord
+#from electionguard.key_ceremony_mediator import KeyCeremonyMediator
 
 # Step 2 - Encrypt Votes
 from electionguard.ballot import (
@@ -32,7 +38,7 @@ from electionguard.encrypt import EncryptionMediator
 
 # Step 3 - Cast and Spoil
 from electionguard.data_store import DataStore
-from electionguard.ballot_box import BallotBox, get_ballots
+from electionguard.ballot_box import BallotBox, get_ballots, submit_ballot
 
 # Step 4 - Decrypt Tally
 from electionguard.tally import (
@@ -68,8 +74,12 @@ from electionguard_tools.helpers.export import (
 )
 
 from electionguard_tools.helpers.election_builder import ElectionBuilder
+from electionguard_tools.factories.election_factory import ElectionFactory
+from electionguard_tools.factories.ballot_factory import BallotFactory
 
 
+logger = logging.getLogger("electionguard")
+logger.setLevel(logging.WARNING)
 
 # Step 0 - Configure Election
 manifest: Manifest
@@ -95,8 +105,15 @@ ciphertext_ballots: List[CiphertextBallot] = []
 # Step 3 - Cast and Spoil
 ballot_store: DataStore[BallotId, SubmittedBallot]
 ballot_box: BallotBox
+ballot_box_ballot: SubmittedBallot
 submitted_ballots: Dict[BallotId, SubmittedBallot]
 
+# Step 4 - Decrypt Tally
+ciphertext_tally: CiphertextTally
+plaintext_tally: PlaintextTally
+plaintext_spoiled_ballots: Dict[str, PlaintextTally]
+decryption_mediator: DecryptionMediator
+lagrange_coefficients: LagrangeCoefficientsRecord
 
 
 def createManifest() -> Manifest:
@@ -106,12 +123,6 @@ def createManifest() -> Manifest:
 	ballot_style_single_vote = BallotStyle("ballot-style-single-vote")
 	ballot_style_single_vote.geopolitical_unit_ids = ["single-vote"]
 	# A contest is a collection of candidates and/or referendum options where the voter may make a selection
-	candidate_ballot_selections = [
-		# "object_id", "sequence_order", "candidate_id"
-		SelectionDescription("candidate-1-selection", 0, "CANDIDATE-1"),
-		SelectionDescription("candidate-2-selection", 1, "CANDIDATE-2"),
-		SelectionDescription("candidate-3-selection", 2, "CANDIDATE-3"),
-	]
 	referendum_ballot_selections = [
     # Referendum selections are simply a special case of `candidate` in the object model
 		SelectionDescription("yes-selection", 0, "YES"),
@@ -119,7 +130,6 @@ def createManifest() -> Manifest:
 		SelectionDescription("abstain-selection", 2, "ABSTAIN"),
 	]
 	referendum_single_vote_contest = ContestDescription("referendum-single-vote",0,"single-vote",VoteVariationType.one_of_m,1,1,"Referendum Single Vote Contest",referendum_ballot_selections,)
-	election_single_vote_contest = ContestDescription("election-single-vote",1,"single-vote",VoteVariationType.one_of_m,1,1,"Election Single Vote Contest",candidate_ballot_selections,)
 	manifest = Manifest(
     spec_version=SpecVersion.EG1_0,
     election_scope_id="esn-manifest",
@@ -135,14 +145,11 @@ def createManifest() -> Manifest:
     ],
     parties=[Party("N/A")],
     candidates=[
-        Candidate("CANDIDATE-1"),
-        Candidate("CANDIDATE-2"),
-        Candidate("CANDIDATE-3"),
         Candidate("YES"),
         Candidate("NO"),
         Candidate("ABSTAIN"),
     ],
-    contests=[referendum_single_vote_contest, election_single_vote_contest],
+    contests=[referendum_single_vote_contest],
     ballot_styles=[ballot_style_single_vote],
 	)
 	
@@ -163,9 +170,13 @@ def createManifest() -> Manifest:
 		return manifest
 	
 def buildElection() -> None:
-	global election_builder, joint_key, commitment, internal, constants
+	global election_builder, joint_key, commitment, internal, constants, device, encrypter, plaintext_ballots, ciphertext_ballots, ballot_box, submitted_ballots, ballot_store, ciphertext_tally, plaintext_tally, plaintext_spoiled_ballots,decryption_mediator,lagrange_coefficients,ballot_box_ballot
+	
+	election_builder = ElectionBuilder(NUMBER_OF_GUARDIANS, QUORUM, manifest)
 	election_builder.set_public_key(hex_to_p(joint_key))
 	election_builder.set_commitment_hash(hex_to_q(commitment))
+
+	
 	internal_manifest, context = get_optional(election_builder.build())
 	constants = create_constants(
     5809605995369958062859502533304574370686975176362895236661486152287203730997089013490246327149176617979883979879665474146725235639037470348294494756855649315623483381532284382829985865022634153578375633183351018921091870438603777296285300005007798125066694887162528853877672530170996389357750706590940588953424536914526358486709688983421547257618538163834967140534985203755017132605692672822700384056871677888971931895590561928921294273236194685526463354686525653654114179791182933523021406079633254465546599561197242920973013822820114343447081949390370753277569017722080197289385153614111846483012304466313627319490653728075778475634253896521588120585027626201092035118691290007496293626942041424997480127725248684036863793198217924529520768114832049450715656899658126803689999781406220992057283178444017598705139849562007174360175183917956509707878788742084753622778956841724673565867858641107428015367836415614107464499199,
@@ -174,6 +185,45 @@ def buildElection() -> None:
     3983976838916583196990814207322510921320016467946417533216999154309346787158720745756708248623554944941202173601872329574166373683909547590349567059887290235949592851779429018520595520234297752071717586124672451539896760499203413787244838790030191769295209586115971156675352412376249718705430315603172039092707904550561519064313499331843446952946564189902025255748380139209555571060994585339534962548340004251462419027674037527180756962099450195292537848904334229156066064209226803823602255101468813019466466208165853013917347257777537304347349735373163087810141891329611178020461116524438016075431298306107501342366508266827438534566861633359768082249446920430439681449905774361889853764938857878702552850700522179471524761958893751061483523094674764719754250107548869925353603911589083937977401963462791699327224813999144150047728455687970145475131554558843766807046314831942872482690705753882915485571147817424402984387346,
 	)
 	
+	device = ElectionFactory.get_encryption_device()
+	encrypter = EncryptionMediator(internal_manifest, context, device)
+	 
+	plaintext_ballots = BallotFactory().generate_fake_plaintext_ballots_for_election(internal_manifest, 5, None, False, False)
+	for plain in plaintext_ballots:
+		for contest in plain.contests:
+			for selection in contest.ballot_selections:
+				if selection.vote == 1:
+					print(f"Ballot ID: {plain.object_id}, Selected: {selection.object_id}")
+		
+	for plain in plaintext_ballots:
+		encrypted_ballot = encrypter.encrypt(plain)
+		ciphertext_ballots.append(get_optional(encrypted_ballot))
+	
+	ballot_store = DataStore()
+	ballot_box = BallotBox(internal_manifest, context, ballot_store)
+	
+
+	for ballot in ciphertext_ballots:
+		# Validity checks dont allow to add via function submit_to_ballotbox. Because constants are different
+		submitted_ballot = ballot_box.cast(ballot)
+		#ballot_box_ballot = submit_ballot(ballot, BallotBoxState.CAST)
+		#ballot_store.set(ballot_box_ballot.object_id, ballot_box_ballot)
+		#submitted_ballot = ballot_store.get(ballot_box_ballot.object_id)
+
+
+	ciphertext_tally = get_optional(tally_ballots(ballot_store, internal_manifest, context))
+	submitted_ballots = get_ballots(ballot_store, BallotBoxState.SPOILED)
+	print(f"cast: {ciphertext_tally.cast()}")
+	print(f"spoiled: {ciphertext_tally.spoiled()}")
+	print(f"Total: {ciphertext_tally}")
+	submitted_ballots_list = list(submitted_ballots.values())
+	decryption_mediator = DecryptionMediator("decryption-mediator",context,)
+	print(context.crypto_extended_base_hash)
+	print(ciphertext_tally.contests.values())
+
+
+
+	
 	
 def on_message(mosq, obj, msg):
 	global commitment, joint_key
@@ -181,10 +231,9 @@ def on_message(mosq, obj, msg):
 	if msg.topic == "joint_key":
 		mosq.unsubscribe("joint_key")
 		buffer = msg.payload
-		joint_key = buffer[:JOINT_KEY_SIZE]
-		commitment = buffer[JOINT_KEY_SIZE:JOINT_KEY_SIZE + COMMITMENT_SIZE]
-		print("Joint Key:", binascii.hexlify(joint_key).decode('utf-8'))
-		print("Commitment:", binascii.hexlify(commitment).decode('utf-8'))
+		joint_key = binascii.hexlify(buffer[:JOINT_KEY_SIZE]).decode('utf-8')
+		commitment = binascii.hexlify(buffer[JOINT_KEY_SIZE:JOINT_KEY_SIZE + COMMITMENT_SIZE]).decode('utf-8')
+		buildElection()
 		
     # This callback will be called for messages that we receive that do not
     # match any patterns defined in topic specific callbacks, i.e. in this case
@@ -193,6 +242,13 @@ def on_message(mosq, obj, msg):
 
 	
 manifest = createManifest()
+
+electionguard.constants.STANDARD_CONSTANTS = create_constants(
+	5809605995369958062859502533304574370686975176362895236661486152287203730997089013490246327149176617979883979879665474146725235639037470348294494756855649315623483381532284382829985865022634153578375633183351018921091870438603777296285300005007798125066694887162528853877672530170996389357750706590940588953424536914526358486709688983421547257618538163834967140534985203755017132605692672822700384056871677888971931895590561928921294273236194685526463354686525653654114179791182933523021406079633254465546599561197242920973013822820114343447081949390370753277569017722080197289385153614111846483012304466313627319490653728075778475634253896521588120585027626201092035118691290007496293626942041424997480127725248684036863793198217924529520768114832049450715656899658126803689999781406220992057283178444017598705139849562007174360175183917956509707878788742084753622778956841724673565867858641107428015367836415614107464499199,
+	115792089237316195423570985008687907853269984665640564039457584007913129639747,  # pow(2, 256) - 189
+	50172736614702193476010968685027850385725009984025754056752351897843540836809501857582622501011043805624882442705121527957457314216703019848506491542416048680150015176751863502045767527184617294497075084910496276997695216972542566310098645323089311784649009927959528878514066350803601544489238112989830818502071033067514117524130744776601302690867843065480705069205775202884218033343131500721992545187412682323348381061850505196518376963582336101914422155939209672462687958072229481112658285666828397567126306616634605158427548433669022805035656789286025402739180974848575290263903285848610951762542336807674715790221593229762571552817379724296274946311992728575342565996306922533232201850750304201504286745236639979257233112859066599580409049888412420870663129590514968528298497578450985177891114800807166167122028381365269811600460476934437467434,
+	3983976838916583196990814207322510921320016467946417533216999154309346787158720745756708248623554944941202173601872329574166373683909547590349567059887290235949592851779429018520595520234297752071717586124672451539896760499203413787244838790030191769295209586115971156675352412376249718705430315603172039092707904550561519064313499331843446952946564189902025255748380139209555571060994585339534962548340004251462419027674037527180756962099450195292537848904334229156066064209226803823602255101468813019466466208165853013917347257777537304347349735373163087810141891329611178020461116524438016075431298306107501342366508266827438534566861633359768082249446920430439681449905774361889853764938857878702552850700522179471524761958893751061483523094674764719754250107548869925353603911589083937977401963462791699327224813999144150047728455687970145475131554558843766807046314831942872482690705753882915485571147817424402984387346,
+	)
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)    
 mqttc.on_message = on_message
